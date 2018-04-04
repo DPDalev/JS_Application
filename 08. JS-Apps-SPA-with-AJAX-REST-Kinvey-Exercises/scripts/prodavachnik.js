af9f934b265f14b43741f72d29cd97bf4a93f903
Dimitar P. Dalev <dpdalev@abv.bg>
2018-04-04 11:09:42 +0300

    Added Book Library and Prodavachnik

---
+++ 08. JS-Apps-SPA-with-AJAX-REST-Kinvey-Exercises/scripts/prodavachnik.js
@@ -0,0 +1,16 @@
+function startApp() {
+
+    $('#linkLogin').show();
+    $('#linkRegister').show();
+    $('#viewLogin').show();
+
+}
+
+/*
+linkHome
+linkLogin
+linkRegister
+linkListAds
+linkCreateAd
+linkLogout
+ */
